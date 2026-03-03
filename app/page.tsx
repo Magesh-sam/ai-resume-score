c84b699195acf1d1e83a6dd7c84d6b83807e6887
mageshkannan <mageshkannanam@gmail.com>
2026-03-03 20:03:17 +0530

    title change in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
-import { ComponentExample } from "@/components/component-example";
 
-export default function Page() {
-return <ComponentExample />;
+function page() {
+  return (
+    <main className="container  grid  justify-center pt-3 min-w-dvw min-h-dvh ">
+        <h1 className='text-3xl'>✨ AI Resume Score</h1>
+    </main>
+  )
 }
+
+export default page
