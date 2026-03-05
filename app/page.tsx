f6bd0057e4f3f2ca1d1406c23e8b605e42ff558a
mageshkannan <mageshkannanam@gmail.com>
2026-03-05 16:58:23 +0530

    add: navbar

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 function page() {
   return (
     <main className="container  grid  justify-center pt-3 min-w-dvw min-h-dvh ">
-        <h1 className='text-3xl'>✨ AI Resume Score</h1>
+      
     </main>
   )
 }
---
+++ components/navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <nav className='w-full flex justify-center items-center p-3'>
+            <h1 className='text-3xl'>✨ AI Resume Score</h1>
+        </nav>
+    )
+}
+
+export default Navbar
